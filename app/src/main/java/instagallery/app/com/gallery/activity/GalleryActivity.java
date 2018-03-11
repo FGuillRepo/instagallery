6c79f83f8748c87010dbb65e72655d1d980cd757
Guillaouic Franck <FOXspin@hotmail.fr>
2018-03-11 17:25:07 +0100

    Create a base GalleryActivity including toolbar and menu.

--- app/src/main/java/instagallery/app/com/gallery/activity/GalleryActivity.java
+++ app/src/main/java/instagallery/app/com/gallery/activity/GalleryActivity.java
@@ -2,19 +2,89 @@ package instagallery.app.com.gallery.activity;
 
 
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.CookieManager;
+import android.webkit.CookieSyncManager;
+import android.widget.TextView;
 
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import de.hdodenhof.circleimageview.CircleImageView;
+import instagallery.app.com.gallery.Model.Data;
 import instagallery.app.com.gallery.R;
 
-public class GalleryActivity extends AppCompatActivity {
+
+
+public class GalleryActivity extends AppCompatActivity  {
+
+    @BindView(R.id.toolbar) Toolbar toolbar;
+    @BindView(R.id.lv_feed) RecyclerView recyclerView;
+    @BindView(R.id.username) TextView username;
+    @BindView(R.id.user_picture) CircleImageView userpicture;
+    @BindView(R.id.swipeRefresh) SwipeRefreshLayout swipeRefreshLayout;
+
+    public static ArrayList<Data> data = new ArrayList<>();
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery);
+        ButterKnife.bind(this);
+        setToolbar();
+
+        // presenter initalize
+
+        if (savedInstanceState == null) {
+        } else {
+        }
 
     }
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.disconnect) {
+            CookieSyncManager.createInstance(this);
+            CookieManager cookieManager = CookieManager.getInstance();
+            cookieManager.removeAllCookie();
+            finish();
+
+            return true;
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void setToolbar(){
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        toolbar.setNavigationIcon(getResources().getDrawable(R.drawable.ic_left_arrow));
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
 
 }
